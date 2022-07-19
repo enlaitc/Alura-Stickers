33053a1abd39438773a0ad3b78990119fed00f2a
enlaitc <thiago021299@hotmail.com>
2022-07-19 15:44:47 -0300

    Aula 1 Lista filmes mais populares do IMDB

---
+++ src/main.java
@@ -0,0 +1,29 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        URI endereco = URI.create("https://imdb-api.com/en/API/MostPopularMovies/k_gxg5mdpc");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(response.body());
+        System.out.println(listaDeFilmes.size());
+
+        for (Map<String, String> filmes : listaDeFilmes) {
+            System.out.println("Titulo: " + filmes.get("title"));
+            System.out.println("Url: " + filmes.get("image"));
+            System.out.println("Rating: "+ filmes.get("imDbRating"));
+            for (int i = 0; i <= Float.parseFloat(filmes.get("imDbRating"))-1; i++){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
